00b85eaca00a228c40bcb14055078c2455f8dc21
MasterKN48 <masterkn48@gmail.com>
2026-03-22 03:31:35 +0530

    chore: remove temporary `test_stdio.js` and `test_stdio2.js` files.

--- apps/server/test_stdio.js
+++
@@ -1,8 +0,0 @@
-import { spawn } from 'node:child_process';
-
-const child = spawn('ls', [], {
-    stdio: ['ignore', 'ignore', 'ignore', 'pipe']
-});
-
-console.log('stdio[3]:', child.stdio[3]);
-process.exit(0);
